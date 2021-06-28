40d8a01978e22fbf8395298e557c3636827a6e9c
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-06-28 13:04:33 +0500

    :white_check_mark: used dotenv module, small changes on imports

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import logging
 
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ContentType, InputFile
-
 import cv2
 
-API_TOKEN = 'YOUR TOKEN'
+from dotenv import load_dotenv
+from os import getenv
+
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram import executor
+
+from aiogram.types import Message
+from aiogram.types import InputFile
+
+
+load_dotenv()
+
+
+API_TOKEN = getenv('BOT_API_TOKEN')
+
+# print(getenv('API_TOKEN'))
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -14,16 +27,19 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
+async def send_welcome(message: Message):
     await message.reply("Hi!\nI'm PictureBot!\nPowered by @Yoshlik_media.")
 
+
 @dp.message_handler()
-async def cont_type_photo(message: types.Message):
+async def cont_type_photo(message: Message):
     img = cv2.imread('down_photos/5415106.jpg')
     cv2.putText(img, message.text, (590, 1050), 7, 8, (255, 206, 92), 3)
     cv2.imwrite("upld_photos/Done.jpg", img)
     photo_bytes = InputFile(path_or_bytesio='upld_photos/Done.jpg')
+
     await bot.send_photo(chat_id=message.from_user.id, photo=photo_bytes, caption=message.text)
 
 if __name__ == '__main__':
